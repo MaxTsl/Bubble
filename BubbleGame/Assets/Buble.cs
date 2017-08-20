e126f6973fa0508f16c4cdcdb273a6c7ff7ea1e4
Max <tsyliakusmaxim@gmail.com>
2017-08-20 20:20:58 +0300

    Add bubble`s creation and randomization

--- BubbleGame/Assets/Buble.cs
+++ BubbleGame/Assets/Buble.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Buble : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    float rotation; //
+    int sign; //знак для поворота по или против часовой стрелки
+
+    // Use this for initialization
+    void Start () {
+        rotation = Random.Range(0.1f, 0.5f);
+
+        float rand = Random.Range(1f, 9f);
+        sign = rand >= 5 ? -1 : 1;
+
+        //пытаемся разнообразить шарики
+        float skale = Random.Range(0.2f, 1.2f);
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1;
+        rb.AddForce(new Vector2(0, Random.Range(1.0f, 7.0f)));
+
+        gameObject.transform.localScale = new Vector3(skale, skale, 0);
+        gameObject.transform.Rotate(new Vector3(skale, skale, 0));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.Rotate(new Vector3(0, 0, sign * rotation));
+    }
 }
--- BubbleGame/Assets/Creation.cs
+++ BubbleGame/Assets/Creation.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Creation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject boubleSfere;
+
+    public float Interval = 0;
+    private float elapsed = 0;
+    // Use this for initialization
+    void Start () {
+        
+        //Через сколько будет создан бервый пузырь
+        Interval = Random.Range(0f, 5f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        elapsed += Time.deltaTime;
+
+        if (elapsed > Interval)
+        {
+            //параметры для создания по времени
+            Interval = Random.Range(0f, 4f);
+            elapsed = 0;
+
+            //-9 - 9 ширина экрана. 5 - половина высоты для камеры по умолчанию
+            GameObject newSfere = Instantiate(boubleSfere, new Vector3(Random.Range(-9.0f, 9.0f), 5, 0), Quaternion.identity);
+
+            Destroy(newSfere, 10f);
+        }
+    }
 }
